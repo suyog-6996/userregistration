501d2b383e10194cfc0596e70bdcb5031cec354e
suyog-6996 <gabhane.suyog@gmail.com>
2020-06-06 11:40:18 +0530

    ADD:Fisrtname

--- user.sh
+++ user.sh
@@ -1,1 +1,15 @@
 echo "WELCOME TO THE USER REGISTRATION PROGRAM"
+firstName()
+{
+read -p "ENTER THE FIRST NAME : " fname
+pat="^[A-Z]{1}[a-z]{2,}$"
+if [[ $fname =~ $pat ]];
+then
+	echo "VALID ";
+else
+	echo "NOTVALID"
+	firstName
+fi
+}
+
+firstName
