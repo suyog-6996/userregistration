d4c0c55be827a761edf59257f468263c3c1cc0f6
suyog-6996 <gabhane.suyog@gmail.com>
2020-06-06 11:45:45 +0530

    ADD:Lastname

--- user.sh
+++ user.sh
@@ -1,4 +1,5 @@
 echo "WELCOME TO THE USER REGISTRATION PROGRAM"
+
 firstName()
 {
 read -p "ENTER THE FIRST NAME : " fname
@@ -12,4 +13,18 @@ else
 fi
 }
 
+lastName()
+{
+read -p "ENTER THE LAST NAME : " lname
+pat="^[A-Z]{1}[a-z]{2,}$"
+if [[ $lname =~ $pat ]];
+then
+	echo "VALID ";
+else
+	echo "INVALID LASTNAME";
+	lastName
+fi
+}
+
 firstName
+lastName
